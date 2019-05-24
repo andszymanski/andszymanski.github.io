fa755f931d93af1c2dde58c08392d430832d7554
andszymanski <andszymanski@github.com>
2019-05-24 14:03:22 +0200

    nowe

--- zadania-zadania-domowe/zadd3-4/js/main.js
+++ zadania-zadania-domowe/zadd3-4/js/main.js
@@ -1,13 +1,13 @@
-let counter = 10;
+// let counter = 10;
 
-let interval = setInterval(countToZero, 1000);
+// let interval = setInterval(countToZero, 1000);
 
-function countToZero(){
-    if(counter == 0){
-        document.getElementById('odliczanie').innerHTML = 'I love CSS :)';
-        clearInterval(interval);
-        return 0;
-    }
-    document.getElementById('odliczanie').innerHTML = counter;
-    counter = counter - 1;
-}
+// function countToZero(){
+//     if(counter == 0){
+//         document.getElementById('odliczanie').innerHTML = 'Kocham Ulę:)';
+//         clearInterval(interval);
+//         return 0;
+//     }
+//     document.getElementById('odliczanie').innerHTML = counter;
+//     counter = counter - 1;
+// }
